984f965498012c1cf3a9f154c12259992984d46c
ParagWadyalkar@1992 <parag.wadyalkar@gmail.com>
2023-02-08 13:01:33 +0530

    test

--- src/test/java/Book/book/MangeTest.java
+++ src/test/java/Book/book/MangeTest.java
@@ -18,12 +18,41 @@ public class MangeTest {
 //	public static void main(String[] args) throws InterruptedException {
 		ChromeOptions options = new ChromeOptions();
 		Map<String, Object> prefs = new HashMap<>();
-		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("start-maximized"); // open Browser in maximized mode
-		options.addArguments("disable-infobars"); // disabling infobars
-		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-gpu"); // applicable to windows os only
+//		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+//		options.addArguments("start-maximized"); // open Browser in maximized mode
+//		options.addArguments("disable-infobars"); // disabling infobars
+//		options.addArguments("--disable-extensions"); // disabling extensions
+//		options.addArguments("--disable-gpu"); // applicable to windows os only
+//		options.addArguments("--no-sandbox"); // Bypass OS security model
+		
+		options.addArguments("--disable-background-networking"); // Bypass OS security model
+		options.addArguments("--disable-background-timer-throttling"); // Bypass OS security model
+		options.addArguments("--disable-client-side-phishing-detection"); // Bypass OS security model
+		options.addArguments("--disable-default-apps"); // Bypass OS security model
+		options.addArguments("--disable-extensions"); // Bypass OS security model
+		options.addArguments("--disable-hang-monitor"); // Bypass OS security model
+		options.addArguments("--disable-popup-blocking"); // Bypass OS security model
+		options.addArguments("--disable-prompt-on-repost"); // Bypass OS security model
+		options.addArguments("--disable-sync"); // Bypass OS security model
+		options.addArguments("--disable-translate"); // Bypass OS security model
+		options.addArguments("--metrics-recording-only"); // Bypass OS security model
+		options.addArguments("--no-first-run"); // Bypass OS security model
+		options.addArguments("--remote-debugging-port=0"); // Bypass OS security model
+		options.addArguments("--safebrowsing-disable-auto-update"); // Bypass OS security model
+		options.addArguments("--enable-automation"); // Bypass OS security model
+		options.addArguments("--password-store=basic"); // Bypass OS security model
+		options.addArguments("--use-mock-keychain"); // Bypass OS security model
+		options.addArguments("--disable-gpu"); // Bypass OS security model
+		options.addArguments("--headless"); // Bypass OS security model
+		options.addArguments("--hide-scrollbars"); // Bypass OS security model
+		options.addArguments("--mute-audio"); // Bypass OS security model
 		options.addArguments("--no-sandbox"); // Bypass OS security model
+		options.addArguments("--disable-dev-shm-usages"); // Bypass OS security model
+		options.addArguments("--window-size=1024,768"); // Bypass OS security model
+		options.addArguments("--remote-debugging-address=0.0.0.0"); // Bypass OS security model
+		options.addArguments("--remote-debugging-port=9222"); // Bypass OS security model
+		
+		
 		prefs.put("profile.default_content_setting_values.notifications", 1);
 		// 1-Allow, 2-Block, 0-default
 		options.setExperimentalOption("prefs", prefs);

300ad3cb7d368008c13e30518549435b63fab460
ParagWadyalkar@1992 <parag.wadyalkar@gmail.com>
2023-02-08 18:45:58 +0530

    ty

--- src/test/java/Book/book/MangeTest.java
+++ src/test/java/Book/book/MangeTest.java
@@ -1,7 +1,9 @@
 package Book.book;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,6 +13,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.netty.channel.pool.FixedChannelPool.AcquireTimeoutAction;
 
 public class MangeTest {
 
@@ -49,34 +52,35 @@ public class MangeTest {
 
 
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get("https://dev.managemedias.com/login");
 		driver.findElement(By.name("email")).sendKeys("vishwas.panke@selftech.in");
 		driver.findElement(By.name("password")).sendKeys("12345678");
 		driver.findElement(By.xpath("(//div[@class=\"col-lg-12 col-md-12\"])[3]/div/button")).click();
-//		driver.findElement(By.id("page-header-user-dropdown")).click();
-//		driver.findElement(By.xpath("//div[@class=\"dropdown-menu dropdown-menu-end show\"]/a")).click();
-//		driver.findElement(By.xpath("//div[@class=\"card\"]/div/ul/li[2]/a")).click();
-//		driver.findElement(By.xpath("(//div[@class=\"row mb-3\"])[1]/div[2]/a")).click();
-//		driver.findElement(By.id("email")).sendKeys("parag.wadyalkar@selftech.in");
-//		driver.findElement(By.id("pass")).sendKeys("parag@1234");
-//		driver.findElement(By.id("loginbutton")).click();
-//		driver.findElement(By.xpath("(//span[@class='x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft'])[2]"))
-//				.click();
-//
-//		driver.findElement(By.id("vertical-menu-btn")).click();
-//		driver.findElement(By.xpath("//div[@id=\"sidebar-menu\"]/ul/ul/li/a")).click();
-//		driver.findElement(By.xpath("//div[@id=\"sidebar-menu\"]/ul/ul/li/ul/li/a")).click();
-//		driver.findElement(By.xpath("(//div[@class=\"container-fluid\"])[1]/div[1]")).click();
-//		Thread.sleep(2000);
-//
-//		driver.findElement(By.name("name")).sendKeys("Demo");
-//		driver.findElement(By.id("message")).sendKeys("20% offer on each product");
-//
-//		WebElement upload_file = driver.findElement(By.id("image"));
-//		upload_file.sendKeys("C:\\Users\\AVITA\\Desktop\\Image\\download (2).jpg");
-//
-//		driver.findElement(By.xpath("(//div[@class=\"modal-footer\"])/button[2]")).click();
-//		driver.close();
+		driver.findElement(By.id("page-header-user-dropdown")).click();
+		driver.findElement(By.xpath("//div[@class=\"dropdown-menu dropdown-menu-end show\"]/a")).click();
+		driver.findElement(By.xpath("//div[@class=\"card\"]/div/ul/li[2]/a")).click();
+		driver.findElement(By.xpath("(//div[@class=\"row mb-3\"])[1]/div[2]/a")).click();
+		driver.findElement(By.id("email")).sendKeys("parag.wadyalkar@selftech.in");
+		driver.findElement(By.id("pass")).sendKeys("parag@1234");
+		driver.findElement(By.id("loginbutton")).click();
+		driver.findElement(By.xpath("(//span[@class='x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft'])[2]"))
+				.click();
+
+		driver.findElement(By.id("vertical-menu-btn")).click();
+		driver.findElement(By.xpath("//div[@id=\"sidebar-menu\"]/ul/ul/li/a")).click();
+		driver.findElement(By.xpath("//div[@id=\"sidebar-menu\"]/ul/ul/li/ul/li/a")).click();
+		driver.findElement(By.xpath("(//div[@class=\"container-fluid\"])[1]/div[1]")).click();
+		Thread.sleep(2000);
+
+		driver.findElement(By.name("name")).sendKeys("Demo");
+		driver.findElement(By.id("message")).sendKeys("20% offer on each product");
+
+		WebElement upload_file = driver.findElement(By.id("image"));
+		upload_file.sendKeys("C:\\Users\\AVITA\\Desktop\\Image\\download (2).jpg");
+
+		driver.findElement(By.xpath("(//div[@class=\"modal-footer\"])/button[2]")).click();
+		driver.close();
 
 	}
 
